e991e948a8e374fb220de0e15f948fcd62682f9e
Brian <brianvu7@gmail.com>
2020-04-28 21:05:17 -0500

    added caching and webpack

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -1,6 +1,26 @@
 let transactions = [];
 let myChart;
 
+let db;
+const request = window.indexedDB.open("budget", 1);
+
+request.onupgradeneeded = function(event) {
+  db = event.target.result;
+  db.createObjectStore("pendingTransactions", { keyPath: "listID", autoIncrement: true });
+};
+
+request.onsuccess = function() {
+  db = request.result;
+
+  if (navigator.onLine) {
+    checkDatabase();
+  }
+};
+
+request.onerror = function(event) {
+  console.log(event);
+};
+
 fetch("/api/transaction")
   .then(response => {
     return response.json();
@@ -144,6 +164,46 @@ function sendTransaction(isAdding) {
   });
 }
 
+function saveRecord(record) {
+  console.log(record);
+
+  const transaction = db.transaction(["pendingTransactions"], "readwrite");
+  const pendingStore = transaction.objectStore("pendingTransactions");
+
+  pendingStore.add({ record });
+}
+
+function checkDatabase() {
+  const transaction = db.transaction(["pendingTransactions"], "readwrite");
+  const pendingStore = transaction.objectStore("pendingTransactions");
+  
+  const getAll = pendingStore.getAll();
+
+  getAll.onsuccess = function() {
+    if (getAll.result.length > 0) {
+      console.log(getAll.result);
+      fetch("/api/transaction/bulk", {
+        method: "POST",
+        body: JSON.stringify(getAll.result),
+        headers: {
+          Accept: "application/json, text/plain, */*",
+          "Content-Type": "application/json"
+        }
+      })
+      .then(response => response.json())
+      .then(() => {
+          const transaction = db.transaction(["pendingTransactions"], "readwrite");
+          const pendingStore = transaction.objectStore("pendingTransactions");
+
+          const deleteAll = pendingStore.clear();
+          deleteAll.onsuccess = () => {
+            console.log("deleted all items");
+          }
+      });
+    }
+  };
+}
+
 document.querySelector("#add-btn").onclick = function() {
   sendTransaction(true);
 };
@@ -151,3 +211,5 @@ document.querySelector("#add-btn").onclick = function() {
 document.querySelector("#sub-btn").onclick = function() {
   sendTransaction(false);
 };
+
+window.addEventListener("online", checkDatabase);
